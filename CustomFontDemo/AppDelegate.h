f0c551717b969259cc92ba450c7a397041a9ce0c
huweidong <407475190@qq.com>
2016-11-03 14:27:27 +0800

    Initial Commit

---
+++ CustomFontDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomFontDemo
+//
+//  Created by huweidong on 3/11/16.
+//  Copyright © 2016年 huweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomFontDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomFontDemo
+//
+//  Created by huweidong on 3/11/16.
+//  Copyright © 2016年 huweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
